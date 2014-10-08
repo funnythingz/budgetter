341742519e5b957bd75b7751bdd82ae9636f0ce3
funnythingz <funnythingz69+github@gmail.com>
2014-10-09 00:28:10 +0900

    Fixed articles of routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,14 @@
 class ArticlesController < ApplicationController
   def view
     @article = Article.find(params[:id])
+    @layout = params[:layout]
+
+    if @layout == 'modal'
+      render :action => 'modal'
+    end
+  end
+
+  def modal
+
   end
 end
