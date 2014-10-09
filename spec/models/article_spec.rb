cfedf5fc4c8b74447305e3997762b10141f7e6f6
funnythingz <funnythingz69+github@gmail.com>
2014-10-09 23:33:36 +0900

    wrote test of Article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Article, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Article, :type => :model do
+
+  describe '#' do
+
+    # TODO: Articleのテストを書いてみる。まだ途中
+    let(:article) {create(:article)}
+
+    it 'should be valid' do
+      expect(article).to be_valid
+    end
+
+  end
 end
