46c18b842c27c54c717cc4f55eac21c98e8dd0dc
funnythingz <funnythingz69+github@gmail.com>
2014-10-25 10:52:43 +0900

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # root
+  get '/', to: 'entry#index'
+
+  # entry
   get '/entry/new', to: 'entry#new'
   match '/entry/:id', to: 'entry#view', via: 'get', as: 'entry', constraints: {id: /[0-9]+/}
 
+  # static pages
   match '/about', to: 'about#view', via: 'get'
 end
