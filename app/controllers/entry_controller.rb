c08b46acc414d698ef1eca559777b8a808a50b7e
funnythingz <funnythingz69+github@gmail.com>
2014-10-26 18:32:36 +0900

    modified entry_controller_spec

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -1,4 +1,10 @@
 class EntryController < ApplicationController
   def view
   end
+
+  def index
+  end
+
+  def new
+  end
 end
--- spec/controllers/entry_controller_spec.rb
+++ spec/controllers/entry_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe EntryController, :type => :controller do
 
-  describe "GET view" do
+  describe "GET root_path" do
     it "returns http success" do
-      get :view
+      get :index
       expect(response).to have_http_status(:success)
     end
   end
